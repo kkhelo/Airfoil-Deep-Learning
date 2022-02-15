f2104a2f33c4646c1059da1aae7778669edbb15d
kkhelo <kevinyu8745@gmail.com>
2022-02-15 22:07:26 +0800

    220215 sdf1 3 levels

--- train_numerical.py
+++ train_numerical.py
@@ -65,16 +65,16 @@ def main():
 
     torch.set_num_threads(10)
 
-    test_name = 'numerical_sdf3_lr000005c_100ep_03acc'
+    test_name = 'numerical_sdf1_lr000005c_100ep_3levelacc'
     
-    dataset_channel = 3
+    dataset_channel = 1
     batch_size = 32
     epoch_num = 100
     lr = 0.00005
     if_scheduler = False
     # model = torch.load('model/3channel_lr00008c_21-50ep.pk1')
     model = ResNet50_PRelu(dataset_channel, 1)
-    acc_threshhold = 0.3
+    # acc_threshhold = 0.3
 
     transform = transforms.Compose([transforms.ToTensor()])
 
@@ -98,7 +98,9 @@ def main():
 
     writer_path = f'log/{test_name}'
     writer_loss = SummaryWriter(os.path.join(writer_path, 'loss/'))
-    writer_acc  =SummaryWriter(os.path.join(writer_path, 'acc/'))
+    writer_acc1  =SummaryWriter(os.path.join(writer_path, 'acc1/'))
+    writer_acc3  =SummaryWriter(os.path.join(writer_path, 'acc3/'))
+    writer_acc5  =SummaryWriter(os.path.join(writer_path, 'acc5/'))
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(device)
@@ -119,7 +121,10 @@ def main():
         print('*' * 25, f'Epoch {epoch+1:03d} Start :', '*' * 25)
 
         training_loss = 0
-        training_acc = 0
+        training_acc1 = 0
+        training_acc3 = 0
+        training_acc5 = 0
+
         model.train()
         for i, (images, labels) in enumerate(train_loader):
             images, labels = images.float().to(device), labels.float().to(device)
@@ -132,8 +137,13 @@ def main():
             optimizer.step()
 
             acc = torch.abs(torch.sub(result, labels))
-            acc = (acc < acc_threshhold).float()
-            training_acc += torch.sum(acc).item()
+
+            acc1 = (acc < 0.1).float()
+            training_acc1 += torch.sum(acc1).item()
+            acc3 = (acc < 0.3).float()
+            training_acc3 += torch.sum(acc3).item()
+            acc5 = (acc < 0.5).float()
+            training_acc5 += torch.sum(acc5).item()
 
             if (not (i+1) % record_batch) or ( i+1 == len(train_loader)):
                 print(f'Epoch : {epoch+1:03d}/{epoch_num:03d} | Batch : {i+1:04d}/{len(train_loader):04d} | Loss : {loss:.2f}')     
@@ -144,7 +154,10 @@ def main():
 
         # Validation 
         validation_loss = 0
-        validation_acc = 0
+        validation_acc1 = 0
+        validation_acc3 = 0
+        validation_acc5 = 0
+
         model.eval()
         with torch.no_grad():
             for i, (images, labels) in enumerate(val_loader):
@@ -155,25 +168,39 @@ def main():
                 validation_loss += loss
 
                 acc = torch.abs(torch.sub(result, labels))
-                acc = (acc < acc_threshhold).float()
-                validation_acc += torch.sum(acc).item()
+                acc1 = (acc < 0.1).float()
+                validation_acc1 += torch.sum(acc1).item()
+                acc3 = (acc < 0.3).float()
+                validation_acc3 += torch.sum(acc3).item()
+                acc5 = (acc < 0.5).float()
+                validation_acc5 += torch.sum(acc5).item()
 
         
-        print(f'Epoch {epoch+1:03d} finished | Traning acc : {training_acc/len(train_set)*100:.2f}% | Validation acc : {validation_acc/len(val_set)*100:.2f}%')
+        # print(f'Epoch {epoch+1:03d} finished | Traning acc : {training_acc1/len(train_set)*100:.2f}% | Validation acc : {validation_acc1/len(val_set)*100:.2f}%')
+        print(f'Epoch {epoch+1:03d} finished | Traning acc : | Validation acc : ')
+        print(f'           acc1 :  |     {training_acc1/len(train_set)*100:.2f}%    |      {validation_acc1/len(val_set)*100:.2f}%')
+        print(f'           acc3 :  |     {training_acc3/len(train_set)*100:.2f}%    |      {validation_acc3/len(val_set)*100:.2f}%')
+        print(f'           acc5 :  |     {training_acc5/len(train_set)*100:.2f}%    |      {validation_acc5/len(val_set)*100:.2f}%')
+
         print(f'Time elapsed : {time.time()/60 - start:.2f} min')
-        
 
         writer_loss.add_scalars('Loss', {'Train' : training_loss/len(train_loader), 'Validation' : validation_loss/len(val_loader)}, epoch + 1)
-        writer_acc.add_scalars('Accuracy', {'Train' : training_acc/len(train_set)*100, 'Validation' : validation_acc/len(val_set)*100}, epoch + 1)
+        writer_acc1.add_scalars('Accuracy', {'Train' : training_acc1/len(train_set)*100, 'Validation' : validation_acc1/len(val_set)*100}, epoch + 1)
+        writer_acc3.add_scalars('Accuracy', {'Train' : training_acc3/len(train_set)*100, 'Validation' : validation_acc3/len(val_set)*100}, epoch + 1)
+        writer_acc5.add_scalars('Accuracy', {'Train' : training_acc5/len(train_set)*100, 'Validation' : validation_acc5/len(val_set)*100}, epoch + 1)
 
     print(f'Training finished!!')
     print(f'Total time : {time.time()/60 - start:.2f} min')
     model_name = f'model/{test_name}.pk1'
     torch.save(model, model_name)
     writer_loss.flush()
-    writer_acc.flush()
+    writer_acc1.flush()
+    writer_acc3.flush()
+    writer_acc5.flush()
     writer_loss.close()
-    writer_acc.close()
+    writer_acc1.close()
+    writer_acc3.close()
+    writer_acc5.close()
 
 
 if __name__ == '__main__':
