5ea7d3539b2420125139594f2faa8cb7e534c0c9
kkhelo <kevinyu8745@gmail.com>
2022-07-26 17:44:00 +0800

    validation dataset, train_unet.py 80 % and others small changes

--- train_unet.py
+++ train_unet.py
@@ -15,7 +15,7 @@ import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
 from torchsummary import summary_string
-from dataset.comAirfoilDataset import ComAirfoilDataset
+from dataset.comAirfoilDataset import ComAirfoilDataset, ValComAirfoilDataset
 import helper.comAiroiflUtils as utils
 from helper.comAiroiflUtils import resultImagesGenerator, logWriter
 from network.UNet import UNet
@@ -25,7 +25,7 @@ from network.UNet import UNet
 # Numbers of training epochs
 epochs = 5000
 # Batch size
-bathcSize = 32
+batchSize = 32
 # Learning rate
 lr = 0.0005
 # Inputs channels, outputs channels
@@ -33,20 +33,102 @@ in_channel, out_channel = 3, 4
 # Channel exponent to control network parameters amount
 expo = 6
 # Network
-network = UNet(in_channel, out_channel, expo)
-result, params_info = summary_string(network, (in_channel, 128, 128))
-print(result)
-# print(params_info)
-with open('test.txt', 'w+') as of:
-    of.write(result)
-# print(network)
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+network = UNet(in_channel, out_channel, expo).to(device)
+networkSummary, _ = summary_string(network, (in_channel, 128, 128), device=device)
+# CPU maximum number
+cpuMax = 12
+torch.set_num_threads(cpuMax)
+# Loss function
+criterion = nn.L1Loss().to(device)
+# Optimizer 
+optimizer = torch.optim.Adam(network.parameters(), lr=lr)
+# Data in validation dataset to demo
+demoindex = 5
 
 ######## Dataset settings ########
 
-dataDir='dataset/OpenFOAM_com_airfoil/'
+# Dataset directory.
+dataDir = 'dataset/OpenFOAM_com_airfoil/train/'
+# Validation dataset directory (optional).
+valDaraDir = 'dataset/OpenFOAM_com_airfoil/val/' 
+# Dataset preprocessing mode
+preprocessingMode = ComAirfoilDataset.OFFSETREMOVAL
+# Dataset usage mode, train or test.
+mode = ComAirfoilDataset.TRAIN
+# Dataset and the train loader declaration.
+dataset = ComAirfoilDataset(dataDir, preprocessingMode, mode)
+trainLoader = DataLoader(dataset, batchSize, shuffle=True)
+valDataset = ValComAirfoilDataset(valDaraDir, dataset)
+valLoader = DataLoader(valDataset, batchSize, shuffle=False)
 
-##################################
+########## Log settings ##########
 
-# dataset = ComAirfoilDataset()
+# Log writer declaration (record basic info).
+textFileWriter = logWriter(logDir='./log/trainingLog/', dataset=dataset)
+# Sumarry writer declaration (record loss per epoch).
+lossHistoryWriter = SummaryWriter(log_dir=f'log/SummaryWriterLog/bs{batchSize}_{epochs}ep/')
+# Write train parameters setting
+trainSetting = [f'Runing epochs : {epochs}', f'Batch size : {batchSize}', f'Learning rate : {lr}']
+trainSetting += [f'Loss function : {criterion._get_name()}', f'Optimizaer : {optimizer.__class__.__name__}']
+textFileWriter.writeLog(trainSetting)
+textFileWriter.writeLog('*' * 64)
+# Write network architecture and number of parameters 
+textFileWriter.writeLog(networkSummary)
+# Images output generator
+imgagesGenerator = resultImagesGenerator(channels=4, resolution=128, root=f'./log/resultImages/{batchSize}_{epochs}/')
 
-# logWriter = logWriter(logDir='./log/trainingLog/', dataset=None)
+######## Training script #########
+
+def train():
+    for epoch in range(epochs):
+        print(f'Starting epoch {epoch+1}/{epochs}')
+        network.train()
+        loss_sum = 0
+
+        for _, data in enumerate(trainLoader):
+            inputs, targets = data
+            inputs, targets = inputs.float.to(device), targets.float.to(device)
+
+            optimizer.zero_grad()
+            outputs = network(inputs)
+            
+            loss = criterion(outputs, targets)
+            loss.backward()
+            loss_sum += loss.item()
+
+            optimizer.step()
+
+        loss_val_sum = 0
+        with network.eval():
+            for i, data in enumerate(valLoader):
+                inputs, targets = data
+                inputs, targets = inputs.float.to(device), targets.float.to(device)
+                outputs = network(inputs)
+            
+                loss = criterion(outputs, targets)
+                loss_val_sum += loss.item()
+
+                if not (epoch+1)%100:
+                    if not i :
+                        demoInputs = inputs.data.cpu().numpy()[demoindex].copy()
+                        demoPrediction = outputs.data.cpu().numpy()[demoindex].copy()
+                        demoGroundTruth = targets.data.cpu().numpy()[demoindex].copy()
+                        _, demoGroundTruth = dataset.recoverTrueValues(demoInputs, demoGroundTruth)
+                        _, demoPrediction = dataset.recoverTrueValues(demoInputs, demoPrediction)
+
+                        folderName = f'epoch_{epoch+1}'
+                        imgagesGenerator.setPredAndGround(demoPrediction, demoGroundTruth, folderName)
+                        imgagesGenerator.predVsGround()
+                        imgagesGenerator.globalDiff()
+
+        loss_sum /= len(trainLoader)
+        loss_val_sum /= len(valLoader)
+
+        print()
+        lossHistoryWriter.add_scalars('Loss', {'Train' : loss_sum, 'Validation' : loss_val_sum}, epoch+1)
+
+            
+if __name__ == '__main__':
+    # train()
+    pass
