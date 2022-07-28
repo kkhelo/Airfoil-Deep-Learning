a2a720449a032e63e238c36d143add260a5eaf74
kkhelo <kevinyu8745@gmail.com>
2022-07-28 17:29:24 +0800

    finished train

--- train_unet.py
+++ train_unet.py
@@ -8,7 +8,7 @@
 #
 ##################
 
-import os, sys, time
+import os, sys, time, datetime
 import numpy as np
 import torch 
 import torch.nn as nn
@@ -23,9 +23,9 @@ from network.UNet import UNet
 ####### Training settings ########
 
 # Numbers of training epochs
-epochs = 5000
+epochs = 200
 # Batch size
-batchSize = 32
+batchSize = 8
 # Learning rate
 lr = 0.0005
 # Inputs channels, outputs channels
@@ -37,7 +37,7 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 network = UNet(in_channel, out_channel, expo).to(device)
 networkSummary, _ = summary_string(network, (in_channel, 128, 128), device=device)
 # CPU maximum number
-cpuMax = 12
+cpuMax = 4
 torch.set_num_threads(cpuMax)
 # Loss function
 criterion = nn.L1Loss().to(device)
@@ -51,7 +51,7 @@ demoindex = 5
 # Dataset directory.
 dataDir = 'dataset/OpenFOAM_com_airfoil/train/'
 # Validation dataset directory (optional).
-valDaraDir = 'dataset/OpenFOAM_com_airfoil/val/' 
+valDataDir = 'dataset/OpenFOAM_com_airfoil/val/' 
 # Dataset preprocessing mode
 preprocessingMode = ComAirfoilDataset.OFFSETREMOVAL
 # Dataset usage mode, train or test.
@@ -59,7 +59,7 @@ mode = ComAirfoilDataset.TRAIN
 # Dataset and the train loader declaration.
 dataset = ComAirfoilDataset(dataDir, preprocessingMode, mode)
 trainLoader = DataLoader(dataset, batchSize, shuffle=True)
-valDataset = ValComAirfoilDataset(valDaraDir, dataset)
+valDataset = ValComAirfoilDataset(valDataDir, dataset)
 valLoader = DataLoader(valDataset, batchSize, shuffle=False)
 
 ########## Log settings ##########
@@ -67,7 +67,7 @@ valLoader = DataLoader(valDataset, batchSize, shuffle=False)
 # Log writer declaration (record basic info).
 textFileWriter = logWriter(logDir='./log/trainingLog/', dataset=dataset)
 # Sumarry writer declaration (record loss per epoch).
-lossHistoryWriter = SummaryWriter(log_dir=f'log/SummaryWriterLog/bs{batchSize}_{epochs}ep/')
+lossHistoryWriter = SummaryWriter(log_dir=f'log/SummaryWriterLog/{network.__class__.__name__}/bs{batchSize}_{epochs}ep/')
 # Write train parameters setting
 trainSetting = [f'Runing epochs : {epochs}', f'Batch size : {batchSize}', f'Learning rate : {lr}']
 trainSetting += [f'Loss function : {criterion._get_name()}', f'Optimizaer : {optimizer.__class__.__name__}']
@@ -76,11 +76,12 @@ textFileWriter.writeLog('*' * 64)
 # Write network architecture and number of parameters 
 textFileWriter.writeLog(networkSummary)
 # Images output generator
-imgagesGenerator = resultImagesGenerator(channels=4, resolution=128, root=f'./log/resultImages/{batchSize}_{epochs}/')
+imgagesGenerator = resultImagesGenerator(channels=4, resolution=128, root=f'./log/resultImages/{batchSize}_batchSize_{epochs}_epochs/')
 
 ######## Training script #########
 
 def train():
+    longPeriodTime = shortPeriodTime= startTime = time.time()
     for epoch in range(epochs):
         print(f'Starting epoch {epoch+1}/{epochs}')
         network.train()
@@ -88,7 +89,7 @@ def train():
 
         for _, data in enumerate(trainLoader):
             inputs, targets = data
-            inputs, targets = inputs.float.to(device), targets.float.to(device)
+            inputs, targets = inputs.float().to(device), targets.float().to(device)
 
             optimizer.zero_grad()
             outputs = network(inputs)
@@ -100,35 +101,54 @@ def train():
             optimizer.step()
 
         loss_val_sum = 0
-        with network.eval():
+        network.eval()
+        with torch.no_grad():
             for i, data in enumerate(valLoader):
                 inputs, targets = data
-                inputs, targets = inputs.float.to(device), targets.float.to(device)
+                inputs, targets = inputs.float().to(device), targets.float().to(device)
                 outputs = network(inputs)
             
                 loss = criterion(outputs, targets)
                 loss_val_sum += loss.item()
 
-                if not (epoch+1)%100:
-                    if not i :
-                        demoInputs = inputs.data.cpu().numpy()[demoindex].copy()
-                        demoPrediction = outputs.data.cpu().numpy()[demoindex].copy()
-                        demoGroundTruth = targets.data.cpu().numpy()[demoindex].copy()
-                        _, demoGroundTruth = dataset.recoverTrueValues(demoInputs, demoGroundTruth)
-                        _, demoPrediction = dataset.recoverTrueValues(demoInputs, demoPrediction)
+                if not i and not (epoch+1)%100 :
+                    demoInputs = inputs.data.cpu().numpy()[demoindex].copy()
+                    demoPrediction = outputs.data.cpu().numpy()[demoindex].copy()
+                    demoGroundTruth = targets.data.cpu().numpy()[demoindex].copy()
+                    _, demoGroundTruth = dataset.recoverTrueValues(demoInputs, demoGroundTruth)
+                    _, demoPrediction = dataset.recoverTrueValues(demoInputs, demoPrediction)
 
-                        folderName = f'epoch_{epoch+1}'
-                        imgagesGenerator.setPredAndGround(demoPrediction, demoGroundTruth, folderName)
-                        imgagesGenerator.predVsGround()
-                        imgagesGenerator.globalDiff()
+                    folderName = f'epoch_{epoch+1}'
+                    imgagesGenerator.setPredAndGround(demoPrediction, demoGroundTruth, folderName)
+                    imgagesGenerator.predVsGround()
+                    imgagesGenerator.globalDiff()
+        
+        loss = loss_sum / len(trainLoader)
+        lossVal = loss_val_sum / len(valLoader)
 
-        loss_sum /= len(trainLoader)
-        loss_val_sum /= len(valLoader)
+        logLine = f'Epoch {epoch+1:04d} finished | Time duration : {(time.time()-shortPeriodTime)/60:.2f} minutes\n'
+        shortPeriodTime = time.time()
+        logLine += f'Traning loss : {loss:.2f} | Validation loss : {lossVal:.2f}'
+        print(logLine)
+        print('-'*30)
+        if not (epoch+1)%100 :
+            textFileWriter.writeLog(f' *** Time duration for last 100 epochs : {(time.time()-longPeriodTime)/60:.2f} minutes *** ')
+            textFileWriter.writeLog(f'Epoch {epoch+1:04d} finished ')
+            textFileWriter.writeLog(f'Traning loss : {loss:.2f} | Validation loss : {lossVal:.2f}')
+            textFileWriter.writeLog('-'*64)
+            longPeriodTime = time.time()
+        lossHistoryWriter.add_scalars('Loss', {'Train' : loss, 'Validation' : lossVal}, epoch+1)
 
-        print()
-        lossHistoryWriter.add_scalars('Loss', {'Train' : loss_sum, 'Validation' : loss_val_sum}, epoch+1)
+    # Record 
+    totalTime = (time.time()-startTime)/60
+    print(f'Training completed | Total time duration : {totalTime:.2f} minutes')
+    nowTime = datetime.datetime.now()
+    nowTime = str(nowTime).split(' ')[-1]
+    nowTime = nowTime.split('.')[0]
+    textFileWriter.writeLog(f'*** Training completed at {nowTime} ***')
+    textFileWriter.writeLog(f'Total training time : {totalTime:.2f} minutes.')
 
-            
+       
 if __name__ == '__main__':
-    # train()
-    pass
+    train()
+    # pass
